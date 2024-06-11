58b944565baf2f871529d2cf558e8f0076c13d5d
berbascum <berbascum@ticv.cat>
2024-06-11 07:10:45 +0200

    Update: docker paths in plugin_build_droidian_adaptation were implemented

--- libs/bdm_plugin_build_droidian_adaptation.sh
+++ libs/bdm_plugin_build_droidian_adaptation.sh
@@ -48,11 +48,35 @@ fn_docker_plugin_container_vars() {
     ## Paths configuration
     SOURCES_FULLPATH="${START_DIR}"
     OUTPUT_FULLPATH="${SOURCES_FULLPATH}/out-${package_name}"
-    PACKAGES_DIR="${OUTPUT_FULLPATH}"
-    buildd_fullpath="${PACKAGES_DIR}" 
-    buildd_sources_fullpath="${SOURCES_FULLPATH}"
+        #PACKAGES_DIR="${OUTPUT_FULLPATH}"
+        #buildd_fullpath="${PACKAGES_DIR}" 
+        #buildd_sources_fullpath="${SOURCES_FULLPATH}"
     ## Create the output dir
-    [ -d "$PACKAGES_DIR" ] || mkdir -v $PACKAGES_DIR
+    [ -d "${OUTPUT_FULLPATH}" ] || mkdir -v "${OUTPUT_FULLPATH}"
+    
+    # Set droidian adaptation specific paths
+    droidian_build_tools_relpath="droidian-build-tools/bin"
+    droidian_build_tools_fullpath="${START_DIR}/${droidian_build_tools_relpath}"
+    adapt_droidian_template_relpath="droidian"
+    #pkg_relpath="droidian/${vendor}/${codename}/packages/adaptation-${vendor}-${codename}"
+    adapt_droidian_apt_reldir="droidian/${vendor}/${codename}/droidian/apt"
+    ## Set droidian adaptation paths for docker container
+    PACKAGE_DIR="${SOURCES_FULLPATH}"
+    RESULT_DIR="${OUTPUT_FULLPATH}"
+    LOCAL_REPO_DIR="${droidian_build_tools_fullpath}/${adapt_droidian_apt_reldir}"
+    ## Set dirs to mount on the docker container
+    buildd_fullpath="${RESULT_DIR}" 
+    buildd_sources_fullpath="${PACKAGE_DIR}"
+    buildd_local_repo_fullpath="${LOCAL_REPO_DIR}"
+    fn_docker_plugin_build_droidian_adapt_tools_prep
+}
+
+fn_docker_plugin_build_droidian_adapt_tools_prep() {
+    [ ! -d "${droidian_build_tools_relpath}" ] \
+	&& WARN "dir ./${droidian_build_tools_relpath} not exist" \
+	&& warn "If you have a backup with the device template created, extract here" \
+	&& warn "or create a new device droidian template!" \
+	&& error "Aborting..."
 }
 
 fn_docker_plugin_container_conf() {
@@ -61,13 +85,14 @@ fn_docker_plugin_container_conf() {
 
 fn_build_package_on_container() {
 ## TODO: Imported from build_debian_package. Needs to be adapted first
-    ABORT "Imported from build_debian_package. Needs to be adapted first"
     ## Create a build launcher and copy to the sources dir
     echo "#!/bin/bash" > ${SOURCES_FULLPATH}/build-debian-package.sh
     echo >> ${SOURCES_FULLPATH}/build-debian-package.sh
     echo "cd /buildd/sources" >> ${SOURCES_FULLPATH}/build-debian-package.sh
     echo >> ${SOURCES_FULLPATH}/build-debian-package.sh
+
     echo "dpkg-buildpackage -us -uc" >> ${SOURCES_FULLPATH}/build-debian-package.sh
+
     ## Set x permissions
     chmod +x ${SOURCES_FULLPATH}/build-debian-package.sh
     docker exec -it $CONTAINER_NAME bash /buildd/sources/build-debian-package.sh
@@ -78,7 +103,7 @@ fn_build_package_on_container() {
 << "IMPORTED_OLD_INITIAL_DROIDIAN-PLUGIN"
 # TODO:
 ## The build adaptation process consists on thre  parts:
-# config: (outside docker) The adaptation scripts are used to configure the build env
+# new-template: (outside docker) The adaptation scripts are used to create a new device temp
  # build: (on docker) execute releng-build-package on a container
  # sign: (outside docker) droidian-build-tools script signs the packages
  # recipes creation: src/build-tools/image.sh found on:
@@ -86,19 +111,7 @@ fn_build_package_on_container() {
  # debs creation: found on:
    # droidian-build-tools/bin/droidian/<vendor>-<code-name>/droidian/apt
     #
-    # Set package paths
-    droidian_build_tools_fullpath="${START_DIR}/droidian-build-tools/bin"
-    adapt_droidian_template_relpath="droidian"
-    package_relpath="droidian/${vendor}/${codename}/packages/adaptation-${vendor}-${codename}"
-    adapt_droidian_apt_reldir="droidian/${vendor}/${codename}/droidian/apt"
-    ## Set paths for docker
-    PACKAGE_DIR="${droidian_build_tools_fullpath}/${package_relpath}"
-    RESULT_DIR="$(mktemp)"
-    LOCAL_REPO_DIR="${droidian_build_tools_fullpath}/${adapt_droidian_apt_reldir}"
-    ## Set dirs to mount on the docker container
-    buildd_fullpath="${RESULT_DIR}" 
-    buildd_sources_fullpath="${PACKAGE_DIR}"
-    buildd_local_repo_fullpath="${LOCAL_REPO_DIR}"
+
     #
     #
     # Configuring the build execution
@@ -143,32 +156,26 @@ fn_build_package_on_container() {
 IMPORTED_OLD_INITIAL_DROIDIAN-PLUGIN
 
 #fn_plugin_build_debian_package() {
-fn_check_for_droidian_build_tools() {
-	echo "TODO"
-	# AQUI
-}
+
 fn_plugin_sub_exec()  {
     ## Check the git workdir status and abort if not clean
     fn_bblgit_workdir_status_check
+
     ## Check if the last commit has a tag
-    fn_bblgit_last_two_tags_check
+#    fn_bblgit_last_two_tags_check
     ## Get package info
-    fn_get_package_info
+#    fn_get_package_info
     ## Build the change log from the git history
-    fn_bblgit_changelog_build
+#    fn_bblgit_changelog_build
     ## Update version and channel on the main src file
        ## Designed for build_debian_package but may be usefull in future
        # fn_update_main_src_file_version_var
     ## Commit the prebuild changes
-exit
-    fn_bblgit_changelog_commit
+#    fn_bblgit_changelog_commit
     ## Copy the package files to the pkg rootfs dir
        ## Designed for build_debian_package but may be usefull in future
        # fn_copy_files_to_pkg_dir
     ## Call build-package
 
-exit
-
     fn_build_package_on_container
-    #fn_build_package
 }
