c5b770cac14daa70b6ec0c959904143d1c8855b1
berbascum <berbascum@ticv.cat>
2024-10-16 15:01:59 +0200

    (docker-images) berb: own folder creation and improvements on dockerfiles and creation scripts

--- docker-images/upload-image-2-github.sh
+++ docker-images/upload-image-2-github.sh
@@ -5,8 +5,10 @@
 # Login GHCR
 GITHUB_TOKEN=""
 USERNAME="berbascum"
-IMAGE_NAME="berb-build-env"
+#IMAGE_NAME="berb-build-env"
+IMAGE_NAME="droidian-build-essential"
 ARCH="arm64"
+SUITE="next"
 
 ## Get images
 #curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/${USERNAME}/packages?package_type=container
@@ -14,9 +16,9 @@ ARCH="arm64"
 ## Get images tags
 #curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/${USERNAME}/packages/container/${IMAGE_NAME}/versions
 
-echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${USERNAME} --password-stdin
+#echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${USERNAME} --password-stdin
 
-docker tag ${IMAGE_NAME}:${SUITE}-${ARCH} ghcr.io/${USERNAME}/${SUITE}-${ARCH}
+docker tag ${IMAGE_NAME}:${SUITE}-${ARCH} ghcr.io/${USERNAME}/${IMAGE_NAME}:${SUITE}-${ARCH}
 
-docker push ghcr.io/${USERNAME}/${SUITE}-${ARCH}
+docker push ghcr.io/${USERNAME}/${IMAGE_NAME}:${SUITE}-${ARCH}
 
